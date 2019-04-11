27e7610c7f2b747d1f14c1c80d5415da44e1835e
Nadav Tasher <tashernadav@gmail.com>
2019-04-11 22:46:07 +0300

    Things

--- javascript/persona/persona.js
+++ javascript/persona/persona.js
@@ -1,20 +1,24 @@
-function persona(callback) {
-    hide("title");
-    hide("description");
-    hide("custom");
-    hide("icons");
-    // Start animations
-    show("title");
-    slide("title", false, () => {
-        show("description");
-        slide("description", true, () => {
-            show("custom");
-            slide("custom", false, () => {
-                show("icons");
-                slide("icons", true, () => {
-                    callback();
+function persona(callback, animate = true) {
+    if (animate) {
+        hide("title");
+        hide("description");
+        hide("custom");
+        hide("icons");
+        // Start animations
+        show("title");
+        slide("title", false, () => {
+            show("description");
+            slide("description", true, () => {
+                show("custom");
+                slide("custom", false, () => {
+                    show("icons");
+                    slide("icons", true, () => {
+                        callback();
+                    });
                 });
             });
         });
-    });
+    } else {
+        callback();
+    }
 }
