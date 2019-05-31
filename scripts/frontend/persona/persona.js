cc973a08374ae5bd2f3d31f8717da704b20e0e10
Nadav Tasher <tashernadav@gmail.com>
2019-05-31 18:25:05 +0300

    Fixes

--- scripts/frontend/persona/persona.js
+++ scripts/frontend/persona/persona.js
@@ -1,23 +1,10 @@
 function persona(callback, animate = true) {
     if (animate) {
-        hide("title");
-        hide("description");
-        hide("custom");
-        hide("icons");
-        // Start animations
-        show("title");
-        slide("title", false, () => {
-            show("description");
-            slide("description", true, () => {
-                show("custom");
-                slide("custom", false, () => {
-                    show("icons");
-                    slide("icons", true, () => {
-                        callback();
-                    });
-                });
-            });
-        });
+        let v = get("icons");
+        for (let i = 0; i < v.children.length; i++) {
+            slideIn(v.children[i], i % 2 === 0);
+        }
+        callback();
     } else {
         callback();
     }
