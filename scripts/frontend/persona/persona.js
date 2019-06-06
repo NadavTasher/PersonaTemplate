1af3eb460ffcab76203ad3cf88fc5f2faf6a55da
Nadav Tasher <tashernadav@gmail.com>
2019-06-06 12:56:00 +0300

    Inherited WAB Update

--- scripts/frontend/persona/persona.js
+++ scripts/frontend/persona/persona.js
@@ -1,9 +1,8 @@
-function persona(callback = undefined, animate = true) {
+function persona(animate = true) {
     if (animate) {
         let v = get("icons");
         for (let i = 0; i < v.children.length; i++) {
             slide(v.children[i], true, i % 2 === 0);
         }
     }
-    if (callback !== undefined) callback();
 }
