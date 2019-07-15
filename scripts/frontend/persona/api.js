b3381087ef4e901e6fcc7e9c7d52507af7a15126
Nadav Tasger <tashernadav@gmail.com>
2019-07-15 23:14:43 +0300

    Update

--- scripts/frontend/persona/api.js
+++ scripts/frontend/persona/api.js
@@ -7,7 +7,7 @@ function persona(animate = true) {
     if (animate) {
         let v = get("icons");
         for (let i = 0; i < v.children.length; i++) {
-            slide(v.children[i], true, i % 2 === 0);
+            slide(v.children[i], IN, i % 2 === 0, 1, i/2);
         }
     }
 }
