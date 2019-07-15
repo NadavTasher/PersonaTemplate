a5faadb8c3454b76c4a732481025f163e1b4e4f9
Nadav Tasger <tashernadav@gmail.com>
2019-07-15 23:21:46 +0300

    Update

--- scripts/frontend/persona/api.js
+++ scripts/frontend/persona/api.js
@@ -5,8 +5,10 @@
 
 function persona(animate = true) {
     if (animate) {
+        let v = get("icons");
+        v.style.opacity = "0";
         transition(document.body, IN, () => {
-            let v = get("icons");
+            v.style.opacity = "1";
             for (let i = 0; i < v.children.length; i++) {
                 slide(v.children[i], IN, i % 2 === 0, 1, i / 2);
             }
