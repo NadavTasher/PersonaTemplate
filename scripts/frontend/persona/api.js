91e0be711011df5ab8f9c192c10e239883e44251
Nadav Tasger <tashernadav@gmail.com>
2019-09-17 22:36:06 +0300

    WAB Update, Renewed Animations

--- scripts/frontend/persona/api.js
+++ scripts/frontend/persona/api.js
@@ -6,12 +6,8 @@
 function persona(animate = true) {
     if (animate) {
         let v = get("icons");
-        v.style.opacity = "0";
-        transition(document.body, IN, () => {
-            v.style.opacity = "1";
-            for (let i = 0; i < v.children.length; i++) {
-                slide(v.children[i], IN, i % 2 === 0, 1, i / 2);
-            }
-        });
+        for (let i = 0; i < v.children.length; i++) {
+            slide(v.children[i], IN, i % 2 !== 0, (i + 1) * 0.5);
+        }
     }
 }
